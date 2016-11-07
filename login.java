dbe60144ec35ca0709eba78a0fabcd53913c49d4
shrebox <shreyash15097@iiitd.ac.in>
2016-11-07 11:47:58 +0530

    login_const_addlabel

--- login.java
+++ login.java
@@ -19,6 +19,13 @@ public class login extends JFrame{
 		startPanel = new JPanel();
 		startPanel.setLayout(new BoxLayout(startPanel, BoxLayout.Y_AXIS));
 		startPanel.setBorder(new EmptyBorder(new Insets(150, 200, 150, 200)));
+
+		title = new JLabel("Facility Management Service!");
+		title.setFont(new Font("Sherif",Font.BOLD,22));
+		title.setAlignmentX(Component.CENTER_ALIGNMENT);
+		startPanel.add(title);
+		
+		startPanel.add(Box.createRigidArea(new Dimension(0, 80)));
 	}
 
 	
