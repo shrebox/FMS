251e186b943b8ad5096e39919680a4545377040d
Tushita08 <tushita15108@iiitd.ac.in>
2016-11-10 23:32:53 +0530

    login_read_update_database

--- login.java
+++ login.java
@@ -10,6 +10,11 @@ import java.util.*;
 
 public class login extends JPanel{
 
+		ArrayList<admin> admin = new ArrayList<admin>();
+		ArrayList<supervisor> supervisors = new ArrayList<supervisor>();
+		ArrayList<staff> staffs = new ArrayList<staff>();
+
+
 	JPanel startPanel;
 	JLabel title,name;
 	JButton login, googlelogin, register;
@@ -52,8 +57,96 @@ public class login extends JPanel{
 		startPanel.add(name);
 
 		add(startPanel);
+
+		loginevent log = new loginevent();
+		login.addActionListener();
 	}
 
-	
+	private class loginevent implements ActionListener{
+		public void actionPerformed(ActionEvent event)
+		{
+
+		}
+	}
+
+public static void updateFile(,int size) throws Exception
+{
+	FileWriter file = new FileWriter("input.txt",false);
+   	BufferedWriter bw = new BufferedWriter(file);
+    PrintWriter pw = new PrintWriter(bw);
+	for(int i=0;i<size;i++)
+		{
+			//pw.print(a.get(i).name +",");
+			//pw.print(a.get(i).post +",");
+			pw.print(a.get(i).username +",");
+			pw.print(a.get(i).password +",");
+			pw.print(a.get(i).uid +",");
+		//	pw.print(a.get(i).size +",");
+		}
+		
+
+	pw.print(personarray[i].status);
+		if(i!= size-1)
+		{
+			pw.print("\n");
+		}
+	}
+
+	pw.close();
+
+}
+
+public static void readfile()
+{
+	FileReader inp = new FileReader("dbqbasics.csv");
+		BufferedReader bfile = new BufferedReader(inp);
+
+		String line= bfile.readLine();
+		int a=0,su=0,st=0;
+		
+		while(line!= null)
+		{
+			List<String> items = Arrays.asList(line.split("\\s*,\\s*"));
+			
+			if(items.get(1).eqauls("admin"))
+			{
+				//admin[i].name = items.get(0);
+				//admin[i].post = items.get(1);
+				admin[i].username = items.get(2); 
+				admin[i].password = items.get(3);
+				admin[i].uid = Integer.parseInt(items.get(4));
+				//admin[i].department = items.get(5);
+				line= bfile.readLine();
+				a++;
+			}
+			else if(items.get(1).eqauls("supervisor"))
+			{
+				//admin[i].name = items.get(0);
+				//admin[i].post = items.get(1);
+				supervisors[i].username = items.get(2); 
+				supervisors[i].password = items.get(3);
+				supervisors[i].uid = Integer.parseInt(items.get(4));	
+				//admin[i].department = items.get(5);
+				line= bfile.readLine();
+				su++;
+			}
+			else if(items.get(1).eqauls("staff"))
+			{
+				//admin[i].name = items.get(0);
+				//admin[i].post = items.get(1);
+				staffs[i].username = items.get(2); 
+				staffs[i].password = items.get(3);
+				staffs[i].uid = Integer.parseInt(items.get(4));
+				//admin[i].department = items.get(5);
+				line= bfile.readLine();
+				st++;
+			}
+			
+			
+		}
+}
+
+
+
 
 }
