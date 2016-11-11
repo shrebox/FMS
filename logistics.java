46e75ae16c03df30ed3d98d2b5a6c7defbba2be3
Tushita08 <tushita15108@iiitd.ac.in>
2016-11-11 23:44:42 +0530

    logistics_function_storeinventory

--- logistics.java
+++ logistics.java
@@ -2,6 +2,10 @@ import java.util.*;
 public class logistics {
 	
 	private String[] equipAvb = new String[100];
+
+	private String username;
+	private String department;
+	private int num;
 	
 	public logistics()
 	{
@@ -13,5 +17,31 @@ public class logistics {
 		return "logistics";
 	}
 	
+	public void storeInventory()
+	{
+		FileReader inp = new FileReader("db4logistic.csv");
+		BufferedReader bfile = new BufferedReader(inp);
+
+		String line= bfile.readLine();
+		int l=0;
+		
+		while(line!= null)
+		{
+			List<String> items = Arrays.asList(line.split("\\s*,\\s*"));
+			
+				spr[l].username = items.get(0); 
+				spr[l].department = items.get(1);
+				spr[l].num = Integer.parseInt(items.get(2));
+
+				/*for(int j=0;j<num;j++)
+				{
+					spr[l].logistics.add(items.get(3+j));
+				}*/
+
+				
+				line= bfile.readLine();
+				l++;
+		}
 
 }
+
