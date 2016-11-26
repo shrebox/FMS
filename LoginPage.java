30030a177afec97158a47cf5b038e1d3ae3e88af
shrebox <shreyash15097@iiitd.ac.in>
2016-11-26 15:46:54 +0530

    LoginPage update(database linking left)

--- LoginPage.java
+++ LoginPage.java
@@ -1,16 +1,14 @@
+import java.awt.FlowLayout ;
 import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionListener ;
 import java.awt.event.ActionEvent ;
 import java.awt.* ;
+import javax.swing.JFrame ;
+import javax.swing.JLabel ; 
 import javax.swing.* ;
 import java.util.*;
 
 public class LoginPage extends JPanel{
-	
-	/*JPanel loginPanel,leftPanel,rightPanel;
-	JLabel userLabel, passLabel;
-	JTextField userF, passF;
-	JButton submit, cancel;*/
 	
 	 	private JLabel labelUsername = new JLabel("Enter username: ");
 	    private JLabel labelPassword = new JLabel("Enter password: ");
@@ -19,62 +17,19 @@ public class LoginPage extends JPanel{
 	    private JButton buttonLogin = new JButton("Login");
 	    private JButton cancelLogin = new JButton("Cancel");
 	    private JButton resetLogin = new JButton("Reset");
+	    private JPanel startPanel;
 	    
-	public LoginPage()
+	    login t;
+	    
+	public LoginPage(login tt)
 	{
-		/*loginPanel = new JPanel();
-		loginPanel.setLayout(new BoxLayout(loginPanel, BoxLayout.X_AXIS));
-		loginPanel.setBorder(new EmptyBorder(new Insets(50, 50, 50, 50)));  
-		
-		add(loginPanel);
-		
-		leftPanel = new JPanel();
-		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
-		leftPanel.setBorder(new EmptyBorder(new Insets(50, 0, 50, 50)));
-		loginPanel.add(leftPanel);
-		
-		rightPanel = new JPanel();
-		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
-		rightPanel.setBorder(new EmptyBorder(new Insets(50, 0, 50, 50)));
-		loginPanel.add(rightPanel);
-		
-		userLabel = new JLabel("Username: ");
-		userLabel.setFont(new Font("Sherif",Font.BOLD,14));
-		userLabel.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		leftPanel.add(userLabel);
-		
-		userF = new JTextField(15);
-		userF.setAlignmentX(Component.LEFT_ALIGNMENT);
-		rightPanel.add(userF);
-		
-		loginPanel.add(Box.createRigidArea(new Dimension(0, 40)));
-		
-		passLabel = new JLabel("Password: ");
-		passLabel.setFont(new Font("Sherif",Font.BOLD,14));
-		passLabel.setAlignmentX(Component.RIGHT_ALIGNMENT);
-		leftPanel.add(passLabel);
-		
-		passF = new JPasswordField(15);
-		passF.setAlignmentX(Component.LEFT_ALIGNMENT);
-		rightPanel.add(passF);
-		
-		loginPanel.add(Box.createRigidArea(new Dimension(0, 40)));
-		
-		submit = new JButton("Submit");
-		submit.setAlignmentX(Component.CENTER_ALIGNMENT);
-		leftPanel.add(submit);
-		
-		cancel = new JButton("Cancel");
-		cancel.setAlignmentX(Component.CENTER_ALIGNMENT);
-		rightPanel.add(cancel);*/
-		
+		t = tt;
 		 JPanel newPanel = new JPanel(new GridBagLayout());
          
 	        GridBagConstraints constraints = new GridBagConstraints();
 	        constraints.anchor = GridBagConstraints.WEST;
 	        constraints.insets = new Insets(5, 0, 10, 0);
-	         
-	        // add components to the panel
+	      
 	        constraints.gridx = 0;
 	        constraints.gridy = 0;     
 	        newPanel.add(labelUsername, constraints);
@@ -105,18 +60,57 @@ public class LoginPage extends JPanel{
 	        constraints.gridy = 2;
 	        constraints.gridwidth = 2;
 	        constraints.anchor = GridBagConstraints.EAST;
-	        newPanel.add(cancelLogin, constraints);
-	         
-	        // set border for the panel
-	        newPanel.setBorder(BorderFactory.createTitledBorder(
-	                BorderFactory.createEtchedBorder(), "Login Panel"));
-	         
-	        // add the panel to this frame
+	       
+	        newPanel.add(cancelLogin, constraints);	
+	       
+	        newPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Login Panel"));
+	        
 	        add(newPanel);
-	         
-	       // pack();
-	        //setLocationRelativeTo(null);
+	        
+	       cancelEvent CE = new cancelEvent();  
+		   cancelLogin.addActionListener(CE);
+		   
+		   resetEvent RE = new resetEvent();  
+		   resetLogin.addActionListener(RE);
+		   
+		   regEvent regE = new regEvent();  
+		   buttonLogin.addActionListener(regE);
 		
+		
+	}
+	
+	public class regEvent implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		
+			t.goStart();
+			revalidate();
+		}
+		
+	}
+	
+	public class cancelEvent implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			textUsername.setText("");
+			fieldPassword.setText("");
+			t.goStart();
+			revalidate();
+		}
+		
+	}
+	
+	public class resetEvent implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+	
+			textUsername.setText("");
+			fieldPassword.setText("");
+		}
 		
 	}
 }
